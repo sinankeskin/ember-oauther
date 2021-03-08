f40d3e6eb4a693c28ee9513ec03218f99e289ead
Sinan Keskin <sinan@kesk.in>
2021-03-08 20:41:56 +0300

    Update addon register

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+/* eslint-disable node/no-extraneous-require */
 'use strict';
+
+const version = require('./package.json').version;
+const writeFile = require('broccoli-file-creator');
+const mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: require('./package').name,
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    app.import('vendor/ember-oauther/register-version.js');
+  },
+
+  treeForVendor() {
+    const content = `Ember.libraries.register('Ember OAuther', '${version}');`;
+    const registerVersionTree = writeFile(
+      'ember-oauther/register-version.js',
+      content
+    );
+
+    return mergeTrees([registerVersionTree]);
+  },
 };
